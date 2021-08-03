39248b4186e3461aafa1294f2b11b91fd38b5eb5
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-08-03 17:07:47 -0500

    Element Insertion
    Did troubleshooting for DOM loading and HTML element insertion. Inserted a button into the DOM.

--- js/video_page_load.js
+++ js/video_page_load.js
@@ -1,1 +1,19 @@
 console.log("Extension script working")
+
+function highlights_button_pressed() {
+	console.log("Highlights button pressed");
+}
+
+var highlights_button = document.createElement("button");
+highlights_button.id = "highlights_button";
+highlights_button.textContent = "LIVESTREAM HIGHLIGHTS";
+highlights_button.addEventListener("click", highlights_button_pressed);
+
+window.onload = function() {
+	const chatframe = document.getElementById("chatframe")
+	chatframe.parentNode.appendChild(highlights_button);
+}
+
+
+
+
