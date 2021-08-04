d729e15a33acb1b7ae351ce5d99f2a84784042b5
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-08-04 13:57:00 -0500

    Initial Continuation ID
    Code for fetching the initial continuation ID for the live chat of a stream.

--- js/video_page_load.js
+++ js/video_page_load.js
@@ -1,7 +1,33 @@
-console.log("Extension script working")
+console.log("Livestream Highlighter: Content script loaded")
+
+//Settings Variables
+
+
+function get_initial_continuation_ID() {
+	fetch(window.location.href)
+	.then(
+		(response) => {
+			if(response.status !== 200) {
+				console.log("Livestream Highlighter: Error on retrieving initial continuation ID. Error code: " + response.status)
+				return;
+			}
+			return response.text();
+		})
+	.then(
+		(data) => {
+			continuationIndex = data.indexOf('"continuation":"') + 16;
+			return data.substring(continuationIndex, data.indexOf('"', continuationIndex));
+		}
+	);
+}
+
+function get_next_continuation() {
+	//TODO: "https://www.youtube.com/live_chat_replay?continuation="
+}
 
 function highlights_button_pressed() {
 	console.log("Highlights button pressed");
+	get_initial_continuation_ID();
 }
 
 var highlights_button = document.createElement("button");
