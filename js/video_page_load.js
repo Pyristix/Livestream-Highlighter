a88cea771ec9e5bf000670d2b323ea2635ac907b
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-08-06 17:13:40 -0500

    Chat Message Collection & Half of Front End
    Added a recursive function to collect all of the chat messages in a given stream. Also did about half of the front end work.

--- js/video_page_load.js
+++ js/video_page_load.js
@@ -1,10 +1,71 @@
 console.log("Livestream Highlighter: Content script loaded")
 
+//0=Initial, 1=Gathering messages, 2=Analyzing messages, 3=Displaying analysis results
+let current_processing_state = 0;
+let menu_is_open = false;
+let settings_menu_is_open = false;
+
+
 //Settings Variables
 
 
-function get_initial_continuation_ID() {
-	fetch(window.location.href)
+//HTML elements
+const highlights_area = document.createElement("div");
+highlights_area.id = "highlights_area";
+
+const highlights_button = document.createElement("button");
+highlights_area.appendChild(highlights_button);
+highlights_button.id = "highlights_button";
+highlights_button.className = "highlights_area_item";
+highlights_button.textContent = "LIVESTREAM HIGHLIGHTS";
+highlights_button.addEventListener("click", highlights_button_pressed);
+
+const highlights_menu = document.createElement("div");
+highlights_menu.id = "highlights_menu";
+highlights_menu.className = "highlights_area_item";
+
+const highlights_menu_status_message = document.createElement("p");
+highlights_menu_status_message.id = "highlights_menu_status_message";
+highlights_menu_status_message.className = "highlights_area_item";
+
+const settings_menu = document.createElement("div");
+settings_menu.id = "settings_menu";
+settings_menu.className = "highlights_area_item";
+
+const settings_button = document.createElement("button");
+highlights_menu.appendChild(settings_button);
+settings_button.id = "settings_button";
+settings_button.textContent = "SETTINGS";
+settings_button.addEventListener("click", () => {
+	settings_menu_is_open = true;
+	highlights_menu.removeChild(settings_button);
+	highlights_area.insertBefore(settings_menu, highlights_menu);
+})
+
+const group_selection_bar = document.createElement("div");
+
+const group_settings_area = document.createElement("div");
+
+const save_settings_button = document.createElement("button");
+settings_menu.appendChild(save_settings_button);
+save_settings_button.textContent = "SAVE SETTINGS";
+
+function update_main_menu() {
+	if(current_processing_state === 1){
+		highlights_menu_status_message.textContent = "Gathering Chat Messages";
+	} else if(current_processing_state === 2) {
+		highlights_menu_status_message.textContent = "Analyzing Chat Messages";
+	} else {
+		highlights_menu_status_message.textContent = "";
+	}
+	highlights_menu.innerHTML = "";
+	highlights_menu.appendChild(settings_button);
+	highlights_menu.appendChild(highlights_menu_status_message);
+}
+
+//Gets the first continuation id from the webpage
+function get_initial_continuation_ID(message_array) {
+	return fetch(window.location.href)
 	.then(
 		(response) => {
 			if(response.status !== 200) {
@@ -15,31 +76,98 @@ function get_initial_continuation_ID() {
 		})
 	.then(
 		(data) => {
-			continuationIndex = data.indexOf('"continuation":"') + 16;
-			return data.substring(continuationIndex, data.indexOf('"', continuationIndex));
+			const continuation_index = data.indexOf('"continuation":"') + 16;
+			return data.substring(continuation_index, data.indexOf('"', continuation_index));
 		}
 	);
 }
 
-function get_next_continuation() {
-	//TODO: "https://www.youtube.com/live_chat_replay?continuation="
+//Recursive function that adds the current continuation's messages onto message_array then continues onto the next continuation.
+function get_next_continuation(continuation_id, message_array, iteration_count) {
+	return fetch("https://www.youtube.com/live_chat_replay?continuation=" + continuation_id)
+	.then(
+		(response) => {
+			if(response.status !== 200) {
+				console.log("Livestream Highlighter: Error on retrieving continuation ID. Error code: " + response.status)
+				return;
+			}
+			return response.text();
+		})
+	.then(
+		(data) => {
+			const chat_object_starting_index = data.indexOf('"liveChatContinuation"', 30259) + 23;
+			let chat_object_ending_index = data.indexOf('"trackingParams"', data.length - 370) - 2;
+			if(chat_object_ending_index === -3)
+				chat_object_ending_index = data.indexOf('};', data.length - 651) - 1;
+			
+			let chat_info = data.substring(chat_object_starting_index, chat_object_ending_index);
+			try{
+				chat_info = JSON.parse(chat_info);
+			} catch(error){
+				console.log(error);
+				console.log("DEBUGGING IT_COUNT: " + iteration_count);
+				console.log("DEBUGGING START_INDEX:" + chat_object_starting_index)
+				console.log("DEBUGGING END_INDEX:" + chat_object_ending_index)
+				console.log("DEBUGGING ID: " + continuation_id);
+				console.log("DEBUGGING TEXT: " + chat_info);
+				return;
+			}
+			
+			for(const chat_item in chat_info.actions) {
+				try{
+					message_array.push(chat_info.actions[chat_item].replayChatItemAction.actions[0].addChatItemAction.item.liveChatTextMessageRenderer);
+				} catch (err) {
+					console.log(err);
+					continue;
+				}
+			}
+			
+			console.log(message_array);
+			
+			try{
+				console.log("[" + iteration_count + "] Continuation ID: " + chat_info.continuations[0].liveChatReplayContinuationData.continuation)
+			} catch(error) {
+				console.log("Reached end of continuations")
+				return;
+			}
+			
+			if(data)
+				get_next_continuation(chat_info.continuations[0].liveChatReplayContinuationData.continuation, message_array, iteration_count + 1);
+		}
+	);
 }
 
-function highlights_button_pressed() {
+async function highlights_button_pressed() {
 	console.log("Highlights button pressed");
-	get_initial_continuation_ID();
+	if(menu_is_open){
+		if(settings_menu_is_open)
+			highlights_area.removeChild(settings_menu);
+		highlights_area.removeChild(highlights_menu);
+		menu_is_open = false;
+	} else {
+		if(settings_menu_is_open)
+			highlights_area.appendChild(settings_menu);
+		highlights_area.appendChild(highlights_menu);
+		menu_is_open = true;
+	}
+	
+	if(current_processing_state === 0){
+		current_processing_state = 1;
+		update_main_menu();
+		let message_array = [];
+		const initial_continuation_id = await get_initial_continuation_ID(message_array);
+		console.log("initial_continuation_id: " + initial_continuation_id);
+		await get_next_continuation(initial_continuation_id, message_array, 0);
+		console.log(message_array);
+		current_processing_state = 2;
+		update_main_menu();
+	} else {
+		console.log("Route 2")
+	}
 }
 
-var highlights_button = document.createElement("button");
-highlights_button.id = "highlights_button";
-highlights_button.textContent = "LIVESTREAM HIGHLIGHTS";
-highlights_button.addEventListener("click", highlights_button_pressed);
-
-window.onload = function() {
-	const chatframe = document.getElementById("chatframe")
-	chatframe.parentNode.appendChild(highlights_button);
-}
-
+const recommended_section = document.getElementById("related");
+recommended_section.parentNode.insertBefore(highlights_area, recommended_section);
 
 
 
