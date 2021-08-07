540bca6c194d2660e1ad2d6e8b57e8839f56ff88
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-08-07 18:41:54 -0500

    Gathering Progress & Save Settings Button
    Created a progress indicator and did more front end work.

--- js/video_page_load.js
+++ js/video_page_load.js
@@ -2,9 +2,11 @@ console.log("Livestream Highlighter: Content script loaded")
 
 //0=Initial, 1=Gathering messages, 2=Analyzing messages, 3=Displaying analysis results
 let current_processing_state = 0;
+
 let menu_is_open = false;
 let settings_menu_is_open = false;
-
+let video_length = null;
+let recommended_section = null;
 
 //Settings Variables
 
@@ -26,7 +28,6 @@ highlights_menu.className = "highlights_area_item";
 
 const highlights_menu_status_message = document.createElement("p");
 highlights_menu_status_message.id = "highlights_menu_status_message";
-highlights_menu_status_message.className = "highlights_area_item";
 
 const settings_menu = document.createElement("div");
 settings_menu.id = "settings_menu";
@@ -43,23 +44,43 @@ settings_button.addEventListener("click", () => {
 })
 
 const group_selection_bar = document.createElement("div");
+settings_menu.appendChild(group_selection_bar);
+group_selection_bar.id = "group_selection_bar";
 
 const group_settings_area = document.createElement("div");
+settings_menu.appendChild(group_settings_area);
+group_settings_area.id = "group_settings_area";
 
 const save_settings_button = document.createElement("button");
-settings_menu.appendChild(save_settings_button);
+group_settings_area.appendChild(save_settings_button);
+save_settings_button.id = "save_settings_button";
 save_settings_button.textContent = "SAVE SETTINGS";
+save_settings_button.addEventListener("click", () => {
+	settings_menu_is_open = false;
+	highlights_area.removeChild(settings_menu);
+	update_main_menu();
+})
 
-function update_main_menu() {
-	if(current_processing_state === 1){
-		highlights_menu_status_message.textContent = "Gathering Chat Messages";
-	} else if(current_processing_state === 2) {
-		highlights_menu_status_message.textContent = "Analyzing Chat Messages";
-	} else {
+function timestamp_to_seconds(timestamp) {
+	timestamp = timestamp.split(":");
+	if(timestamp.length === 2)
+		return parseInt(timestamp[0])*60 + parseInt(timestamp[1]);
+	if(timestamp.length === 3)
+		return parseInt(timestamp[0])*60*60 + parseInt(timestamp[1])*60 + parseInt(timestamp[2]);
+}
+
+//Updates what's shown on the main menu according to the current processing state
+function update_main_menu(latest_message_time) {
+	if(current_processing_state === 1)
+		highlights_menu_status_message.textContent = (video_length && latest_message_time) ? "Gathering chat messages: " + ((latest_message_time/video_length)*100).toPrecision(3) + "%" : "Gathering chat messages";
+	else if(current_processing_state === 2)
+		highlights_menu_status_message.textContent = "Analyzing live chat...";
+	else
 		highlights_menu_status_message.textContent = "";
-	}
+	
 	highlights_menu.innerHTML = "";
-	highlights_menu.appendChild(settings_button);
+	if(!settings_menu_is_open)
+		highlights_menu.appendChild(settings_button);
 	highlights_menu.appendChild(highlights_menu_status_message);
 }
 
@@ -123,6 +144,8 @@ function get_next_continuation(continuation_id, message_array, iteration_count) 
 			}
 			
 			console.log(message_array);
+			const latest_message_time = timestamp_to_seconds(message_array[message_array.length - 1].timestampText.simpleText);
+			update_main_menu(latest_message_time);
 			
 			try{
 				console.log("[" + iteration_count + "] Continuation ID: " + chat_info.continuations[0].liveChatReplayContinuationData.continuation)
@@ -132,7 +155,7 @@ function get_next_continuation(continuation_id, message_array, iteration_count) 
 			}
 			
 			if(data)
-				get_next_continuation(chat_info.continuations[0].liveChatReplayContinuationData.continuation, message_array, iteration_count + 1);
+				return get_next_continuation(chat_info.continuations[0].liveChatReplayContinuationData.continuation, message_array, iteration_count + 1);
 		}
 	);
 }
@@ -166,8 +189,19 @@ async function highlights_button_pressed() {
 	}
 }
 
-const recommended_section = document.getElementById("related");
-recommended_section.parentNode.insertBefore(highlights_area, recommended_section);
+try{
+	video_length = timestamp_to_seconds(document.getElementsByClassName("ytp-time-duration")[0].textContent);
+} catch (error) {
+	console.log(error);
+}
+
+try{
+	recommended_section = document.getElementById("related");
+	document.getElementById("related").parentNode.insertBefore(highlights_area, recommended_section);
+} catch(error) {
+	console.log(error);
+}
+
 
 
 
