9efcc9f5b6611079e98d3742c5d51ef4d654f5ab
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-08-08 17:10:42 -0500

    Default Settings & State Structure Changes
    Planned out what settings will be included, how different systems will work, and changed the structure of the extension's states to match.

--- js/video_page_load.js
+++ js/video_page_load.js
@@ -1,7 +1,9 @@
 console.log("Livestream Highlighter: Content script loaded")
 
-//0=Initial, 1=Gathering messages, 2=Analyzing messages, 3=Displaying analysis results
-let current_processing_state = 0;
+//0=Initial, 1=Gathering messages, 2=Messages gathered
+let current_gathering_state = 0;
+//0=Initial, 1=Analyzing messages, 2=Analysis finished
+let current_analysis_state = 0;
 
 let menu_is_open = false;
 let settings_menu_is_open = false;
@@ -9,7 +11,8 @@ let video_length = null;
 let recommended_section = null;
 
 //Settings Variables
-
+let settings_groups = {};
+const default_settings_groups = {"Default": {"Enabled": true, "String/Regex": "Regex", "Text": "草", "Time before trend": 15, "Sensitivity": 0.5}};
 
 //HTML elements
 const highlights_area = document.createElement("div");
@@ -71,9 +74,9 @@ function timestamp_to_seconds(timestamp) {
 
 //Updates what's shown on the main menu according to the current processing state
 function update_main_menu(latest_message_time) {
-	if(current_processing_state === 1)
+	if(current_gathering_state === 1)
 		highlights_menu_status_message.textContent = (video_length && latest_message_time) ? "Gathering chat messages: " + ((latest_message_time/video_length)*100).toPrecision(3) + "%" : "Gathering chat messages";
-	else if(current_processing_state === 2)
+	else if(current_analysis_state === 1)
 		highlights_menu_status_message.textContent = "Analyzing live chat...";
 	else
 		highlights_menu_status_message.textContent = "";
@@ -82,6 +85,14 @@ function update_main_menu(latest_message_time) {
 	if(!settings_menu_is_open)
 		highlights_menu.appendChild(settings_button);
 	highlights_menu.appendChild(highlights_menu_status_message);
+}
+
+function append_highlight_moment() {
+	const highlight_moment = document.createElement("a");
+	highlight_moment.textContent = "a_highlight";
+	highlight_moment.href = "";
+
+	highlights_menu.appendChild(highlight_moment);
 }
 
 //Gets the first continuation id from the webpage
@@ -144,13 +155,18 @@ function get_next_continuation(continuation_id, message_array, iteration_count) 
 			}
 			
 			console.log(message_array);
-			const latest_message_time = timestamp_to_seconds(message_array[message_array.length - 1].timestampText.simpleText);
-			update_main_menu(latest_message_time);
+			try {
+				const latest_message_time = timestamp_to_seconds(message_array[message_array.length - 1].timestampText.simpleText);
+				update_main_menu(latest_message_time);
+			} catch (error) {
+				console.log(error);
+			}
 			
 			try{
 				console.log("[" + iteration_count + "] Continuation ID: " + chat_info.continuations[0].liveChatReplayContinuationData.continuation)
 			} catch(error) {
 				console.log("Reached end of continuations")
+				current_gathering_state = 2;
 				return;
 			}
 			
@@ -174,33 +190,39 @@ async function highlights_button_pressed() {
 		menu_is_open = true;
 	}
 	
-	if(current_processing_state === 0){
-		current_processing_state = 1;
+	if(current_gathering_state === 0){
+		current_gathering_state = 1;
 		update_main_menu();
 		let message_array = [];
 		const initial_continuation_id = await get_initial_continuation_ID(message_array);
 		console.log("initial_continuation_id: " + initial_continuation_id);
 		await get_next_continuation(initial_continuation_id, message_array, 0);
 		console.log(message_array);
-		current_processing_state = 2;
+		//Do analysis here
+		//...
+		//current_analysis_state = 2;
 		update_main_menu();
+		append_highlight_moment();
 	} else {
 		console.log("Route 2")
 	}
 }
 
-try{
-	video_length = timestamp_to_seconds(document.getElementsByClassName("ytp-time-duration")[0].textContent);
-} catch (error) {
-	console.log(error);
-}
+window.addEventListener("load", () => {
+	try{
+		video_length = timestamp_to_seconds(document.getElementsByClassName("ytp-time-duration")[0].textContent);
+	} catch (error) {
+		console.log(error);
+	}
+	
+	try{
+		recommended_section = document.getElementById("related");
+		document.getElementById("related").parentNode.insertBefore(highlights_area, recommended_section);
+	} catch(error) {
+		console.log(error);
+	}
+}, false);
 
-try{
-	recommended_section = document.getElementById("related");
-	document.getElementById("related").parentNode.insertBefore(highlights_area, recommended_section);
-} catch(error) {
-	console.log(error);
-}
 
 
 
