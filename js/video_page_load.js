e2335e7f4777cdb6185cfedf2063bc10f982ff83
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-08-16 21:42:57 -0500

    Time Debug, Timestamp Functionality, Settings Retrieval
    Fixed an issue with advertisement length being taken for video length. Also gave full functionality to the clicking of the timestamps of the highlights. Settings are also now retrieved from local storage on page load.

--- js/video_page_load.js
+++ js/video_page_load.js
@@ -1,9 +1,6 @@
 console.log("Livestream Highlighter: Content script loaded")
 
-let root_url = window.location.href.split("?");
-for(const i in root_url)
-	if(root_url[i].substr(0,2) === "v=")
-		root_url = root_url[0] + "?" + root_url[i];
+let root_url = "https://www.youtube.com/watch?v=" + window.location.href.split("v=")[1].split("&")[0];
 
 //0=Initial, 1=Gathering messages, 2=Messages gathered
 let current_gathering_state = 0;
@@ -12,32 +9,34 @@ let current_analysis_state = 0;
 
 let menu_is_open = false;
 let settings_menu_is_open = false;
-const video_length = timestamp_to_seconds(document.getElementsByClassName("ytp-time-duration")[0].textContent);;
-console.log("video_length: " + video_length);
+let video_length = null;
 let recommended_section = null;
 let message_array = [];
 let latest_gathering_message_time = null;
 //Format: {"group":"Kusa", "start_time":10, "end_time": 40}
 let analysis_results = [];
 
+//Loads previously analyzed results
 chrome.storage.local.get("livestream_highlighter_analysis_results", (results) => {
 	console.log(results)
 	if(results["livestream_highlighter_analysis_results"] !== null && results["livestream_highlighter_analysis_results"][0] === root_url){
 		current_gathering_state = 2;
 		current_analysis_state = 2;
 		analysis_results = results["livestream_highlighter_analysis_results"][1]
+		
+		//Resets the variable for determining whether or not you came to the page from clicking a highlight timestamp
+		chrome.storage.local.get("livestream_highlighter_timestamp_click", (results) => {
+			if(results["livestream_highlighter_timestamp_click"] === true){
+				chrome.storage.local.set({"livestream_highlighter_timestamp_click" : false});
+				highlights_button_pressed();
+			}
+		})
 	}
 	//chrome.storage.local.set({"livestream_highlighter_analysis_results": null});
 	//chrome.storage.local.set({"livestream_highlighter_analysis_results": [root_url, analysis_results]});
 })
 
-//Resets the variable for determining whether or not you came to the page from clicking a highlight timestamp
-chrome.storage.local.get("livestream_highlighter_timestamp_click", (results) => {
-	if(results["livestream_highlighter_timestamp_click"] === true){
-		results["livestream_highlighter_timestamp_click"] = false;
-		highlights_button_pressed();
-	}
-})
+
 
 //DEBUGGING USE
 let analysis_time_changes = [0, 0, 0, 0, 0, 0];
@@ -56,15 +55,31 @@ const default_settings_groups = [{"Name": "Default",
 								  "Regex filter": new RegExp(""), 
 								  "Text to match": [{"String/Regex": "String", "Text": "待機"},
 													{"String/Regex": "String", "Text": "ちょこん"}]}];
-const default_settings_sets = [default_settings_groups];
+
+const default_settings_sets = [{"name" : "Default", 
+								"groups" : default_settings_groups}];
 
 //A group is a collection of text to look for and that collection's associated settings.
 //A set is a group of groups, used for if you want different settings between different types of livestreams
-let settings_groups = [];
 let settings_sets = [];
+let settings_groups = [];
+
+//Retrieves stored settings
+//Format of the settings array in storage should be [int for the index of the set in use, {settings_sets}]
+chrome.storage.local.get("livestream_highlighter_settings", (results) => {
+	console.log(results)
+	if(results["livestream_highlighter_settings"].length){
+		settings_sets = results["livestream_highlighter_settings"][1];
+		settings_groups = settings_sets[results["livestream_highlighter_settings"][0]]["groups"];
+	}
+})
+
+chrome.storage.local.set({"livestream_highlighter_settings" : [0, default_settings_sets]})
 
 //DEBUGGING USE
-settings_groups = default_settings_groups;
+
+
+
 
 
 //HTML elements
@@ -99,6 +114,10 @@ settings_button.addEventListener("click", () => {
 	highlights_area.insertBefore(settings_menu, highlights_menu);
 })
 
+const set_selection_bar = document.createElement("div");
+settings_menu.appendChild(set_selection_bar);
+set_selection_bar.id = "set_selection_bar";
+
 const group_selection_bar = document.createElement("div");
 settings_menu.appendChild(group_selection_bar);
 group_selection_bar.id = "group_selection_bar";
@@ -114,7 +133,7 @@ save_settings_button.textContent = "DONE";
 save_settings_button.addEventListener("click", () => {
 	settings_menu_is_open = false;
 	highlights_area.removeChild(settings_menu);
-	chrome.storage.local.set({"livestream_highlighter_analysis_results": null});
+	//chrome.storage.local.set({"livestream_highlighter_analysis_results": null});
 	//TODO: Reanalyze on settings changes
 	update_main_menu();
 })
@@ -154,12 +173,23 @@ function seconds_to_timestamp(seconds) {
 		return seconds_minutes_hours[2] + ":" + seconds_minutes_hours[1] + ":" + seconds_minutes_hours[0];
 }
 
+function update_settings_menu() {
+	settings_menu.innerHTML = "";
+	
+	settings_menu.appendChild(set_selection_bar);
+	settings_menu.appendChild(group_selection_bar);
+	settings_menu.appendChild(group_settings_area);
+	
+	//for(let i = 0; i < settings_sets.length; )
+	//TODO: Refresh settings menu in all areas.
+}
+
 //Updates what's shown on the main menu according to the current gathering and analysis states
 function update_main_menu(current_analysis_message_time) {
 	if(current_gathering_state === 1)
-		highlights_menu_status_message.textContent = (latest_gathering_message_time) ? "Gathering chat messages: " + ((latest_gathering_message_time/video_length)*100).toPrecision(3) + "%" : "Gathering chat messages";
+		highlights_menu_status_message.textContent = (latest_gathering_message_time && video_length) ? "Gathering chat messages: " + ((latest_gathering_message_time/video_length)*100).toPrecision(3) + "%" : "Gathering chat messages";
 	else if(current_analysis_state === 1)
-		highlights_menu_status_message.textContent = (current_analysis_message_time) ? "Analyzing live chat " + ((current_analysis_message_time/video_length)*100).toPrecision(3) + "%" : "Analyzing live chat...";
+		highlights_menu_status_message.textContent = (current_analysis_message_time && video_length) ? "Analyzing live chat " + ((current_analysis_message_time/video_length)*100).toPrecision(3) + "%" : "Analyzing live chat...";
 	else
 		highlights_menu_status_message.textContent = "";
 	
@@ -184,12 +214,14 @@ function append_highlight_moments() {
 		for(const group_index in settings_groups){
 			if(settings_groups[group_index]["Name"] === analysis_results[index].group){
 				timestamp_link.textContent = seconds_to_timestamp(analysis_results[index].start_time - settings_groups[group_index]["Time before trend"]) + " - " + seconds_to_timestamp(analysis_results[index].end_time);
-				//TODO: onClick for timestamps. chrome.storage.local.set({"livestream_highlighter_timestamp_click" : true});
+				timestamp_link.href = "https://youtu.be/" + root_url.split("?v=")[1] + "?t=" + (analysis_results[index].start_time - settings_groups[group_index]["Time before trend"]);
 				break;
 			}
 		}
 		timestamp_link.className = "timestamp_link";
-		timestamp_link.href = "";
+		timestamp_link.addEventListener("click", () => {
+			chrome.storage.local.set({"livestream_highlighter_timestamp_click" : true});
+		});
 		
 		const highlight_group = document.createElement("p");
 		highlight_moment.appendChild(highlight_group);
@@ -570,26 +602,42 @@ async function highlights_button_pressed() {
 	}
 }
 
-//Sometimes, DOM insertion fails because the page loads too slowly. This listener  
-//prevents this by checking for failure twice a second for 30 seconds
-window.addEventListener("load", () => {
-	if(!document.getElementById("highlights_area") && document.getElementById("chat") && document.getElementById("comments")){
-		console.log("Livestream Highlighter: Inserting Highlights Area");
-		recommended_section = document.getElementById("related");
-		document.getElementById("related").parentNode.insertBefore(highlights_area, recommended_section)
-	}
-	
-	let retry_count = 0;
-	var retry_interval = setInterval(() => {
-		if(!document.getElementById("highlights_area") && document.getElementById("chat") && document.getElementById("comments")){
-			console.log("Livestream Highlighter: Inserting Highlights Area");
-			recommended_section = document.getElementById("related");
-			document.getElementById("related").parentNode.insertBefore(highlights_area, recommended_section)
+
+
+
+//Initial insertion of elements into DOM and retrieval of video length from DOM
+if(!document.getElementById("highlights_area") && !document.getElementsByClassName("ytp-ad-module")[0].children.length && document.getElementById("chat") && document.getElementById("comments")){
+	console.log("Livestream Highlighter: Inserting Highlights Area");
+	recommended_section = document.getElementById("related");
+	document.getElementById("related").parentNode.insertBefore(highlights_area, recommended_section)
+	video_length = timestamp_to_seconds(document.getElementsByClassName("ytp-time-duration")[0].textContent);
+	console.log("video_length: " + video_length);
+}
+
+//Sometimes, DOM insertion fails because the page loads too slowly. The follow code block
+//prevents this by checking for failure twice a second for 30 seconds. It also deals with
+//ad length being confused for video length.
+let retry_count = 0;
+let ad_playing = false;
+var retry_interval = setInterval(() => {
+	if(document.getElementsByClassName("ytp-ad-module")[0].children.length){
+		ad_playing = true;
+		video_length = null;
+	} else {
+		if(!ad_playing){
+			if(!document.getElementById("highlights_area") && document.getElementById("chat") && document.getElementById("comments") && !document.getElementsByClassName("ytp-ad-module")[0].children.length){
+				console.log("Livestream Highlighter: Inserting Highlights Area");
+				recommended_section = document.getElementById("related");
+				document.getElementById("related").parentNode.insertBefore(highlights_area, recommended_section)
+				video_length = timestamp_to_seconds(document.getElementsByClassName("ytp-time-duration")[0].textContent);
+				console.log("Livestream Highlighter: video_length: " + video_length);
+			}
+			retry_count++;
+			if(retry_count >= 60)
+				window.clearInterval(retry_interval);
 		}
-		retry_count++;
-		if(retry_count >= 60)
-			window.clearInterval(retry_interval);
-	}, 500);
-});
+		ad_playing = false;
+	}
+}, 500);
 
 
